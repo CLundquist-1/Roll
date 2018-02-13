8437a0203cf48cca0cf239143b89d1601c315a73
Colt Lundquist <CLundquist-1@outlook.com>
2018-02-12 21:19:36 -0700

    A green windows with input processing function, graphics clearing and double buffering

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -1,10 +1,19 @@
 #include<GL/glew.h>
 #include<GLFW\glfw3.h>
+#include<GL\freeglut.h>
 
 #include<iostream>
 
 using std::cout;
 using std::endl;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int scr_width = 800;
+const unsigned int scr_height = 600;
+
 
 int main() {
 	if (!glfwInit()) {
@@ -20,13 +29,17 @@ int main() {
 
 	// Open a window and create its OpenGL context
 	GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
-	window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+	window = glfwCreateWindow(scr_width, scr_height, "Tutorial 01", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window); // Initialize GLEW
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	// Initialize GLEW
 	glewExperimental = true; // Needed in core profile
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
@@ -40,6 +53,11 @@ int main() {
 	do {
 		// Draw nothing, see you in tutorial 2 !
 
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);			//There is also a depth buffer bit and stencil buffer bit
+
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -48,4 +66,25 @@ int main() {
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 		glfwWindowShouldClose(window) == 0);
 	return 0;
+
+	/////////////////////////////Render Loop///////////////////////////////////////
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
